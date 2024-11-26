ce20bdc2f57c327089ab6025592db3f290cc488b
gagamora <cipcipik@cipcipik-MacBook-Pro.local>
2024-11-26 21:01:13 +0300

    ver 1.0.0 I made navigation and xml layouts

---
+++ app/src/main/java/com/example/smurfs/Intro.java
@@ -0,0 +1,20 @@
+package com.example.smurfs;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import java.lang.reflect.Constructor;
+
+public class Intro extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.intro, container, false);
+
+        return view;
+    }
+}
